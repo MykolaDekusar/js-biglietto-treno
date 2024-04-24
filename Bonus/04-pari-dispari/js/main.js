3245b662461f48dab6bff495d8bd399bdd08069f
MykolaDekusar <ndekusar97@gmail.com>
2024-04-24 17:10:48 +0200

    [FORTMATTED]Formatted code with improvements

--- Bonus/04-pari-dispari/js/main.js
+++ Bonus/04-pari-dispari/js/main.js
@@ -3,21 +3,18 @@ let totNumber = 0;
 let userNumber = 0;
 const pcNumber = Math.ceil(Math.random() * 9);//genero numero casuale compreso tra 1 e 9
 const pari = "pari";
-const dispari = "dispari"
+const dispari = "dispari";
 console.log(pcNumber);
 // chiedere all'utente se vuole pari o dispari
 const userChoice = prompt("Inserisci : pari o dispari?");
 if ((userChoice === pari) || (userChoice === dispari)) {  //verifico che l'utente abbia inserito il valore valido
     console.log(userChoice);
-    userNumber = prompt("Inserisci un numero compreso tra 1 e 9");
-    userNumber = Math.round(Number(userNumber));//trasformo la stringa inserita dall'utente in un numero intero
+    userNumber = Math.round(Number(prompt("Inserisci un numero compreso tra 1 e 9")));//trasformo la stringa inserita dall'utente in un numero intero
     if (!(userNumber < 1) && !(userNumber > 9) && !(isNaN(userNumber))) {  //verifico che l'utente abbia inserito un numero valido
         console.log(userNumber);
         alert(`Hai scelto ${userChoice} e il numero ${userNumber}, il computer ha giocato ${pcNumber}`);//faccio vedere all'utente cos'ha giocato il computer
         totNumber = userNumber + pcNumber;
-        if (totNumber % 2 === 0 && userChoice === pari) {
-            alert("Congratulazioni hai vinto!!!");
-        } else if (totNumber % 2 !== 0 && userChoice === dispari) {
+        if ((totNumber % 2 === 0 && userChoice === pari) || (totNumber % 2 !== 0 && userChoice === dispari)) {
             alert("Congratulazioni hai vinto!!!");
         } else {
             alert("Mi dispiace ma hai perso!!!");
--- scripts/script.js
+++ scripts/script.js
@@ -9,17 +9,13 @@ const discountU18 = 20;
 const discountO65 = 40;
 
 // - Creo una variabile e Chiedo tramite prompt all'utente il numero di km da percorrere  (verifico che sia effettivamente un numero e lo converto da stringa)
-let userDistance = prompt('Quanti Km vuoi percorrere?');
-console.log(userDistance);
-userDistance = Number(userDistance);
+const userDistance = Number(prompt('Quanti Km vuoi percorrere?'));
 console.log(userDistance);
 if (isNaN(userDistance) || userDistance <= 0 || userDistance === null) { //Verifico che la distanza inserita sia valida
     alert('ERRORE Inserisci una distanza valida');//Se la distanza non è valida allora esci
 } else {
     // - Creo una variabile e Chiedo tramite prompt all'utente l'età  (verifico che sia effettivamente un numero e lo converto da stringa)
-    let userAge = prompt('Quanti anni hai?');
-    console.log(userAge);
-    userAge = Math.floor(parseInt(userAge)); //Porto al numero minore se l'utente dovesse inserire un'età con decimali
+    const userAge = Math.floor(Number(prompt('Quanti anni hai?')));//Porto al numero minore se l'utente dovesse inserire un'età con decimali
     console.log(userAge);
     if (isNaN(userAge) || userAge < 0 || userDistance === null) { //Verifico che l'età inserita sia valida
         alert('ERRORE Inserisci un età valida');//Se l'età non è valida esco sennò continuo
@@ -27,27 +23,22 @@ if (isNaN(userDistance) || userDistance <= 0 || userDistance === null) { //Verif
         // - Verifico se l'utente rientra nella fascia <18 anni, e se rientra allora applico uno sconto del 20% al prezzo del biglietto
         if (userAge < 18) {
             ticketPrice = (userDistance * kmPrice);
-            // - Calcolo il prezzo del biglietto meno lo sconto applicato (in questo caso quello del 20%)
-            ticketPrice = ticketPrice - (ticketPrice * (discountU18 / 100));
             console.log(ticketPrice);
-            // - Grazie all' uso di toFixed 2 posso creare l'output fino a 2 cifre decimali in questo caso
-            ticketPrice = Math.round(ticketPrice * 100) / 100;
+            // - Calcolo il prezzo del biglietto meno lo sconto applicato (in questo caso quello del 20%)
+            ticketPrice = Math.round((ticketPrice - (ticketPrice * (discountU18 / 100))) * 100) / 100;
             console.log(ticketPrice);
             alert(`Abbiamo applicato lo sconto del ${discountU18}%. Il prezzo del biglietto è di ${ticketPrice}€`);
             // - Verifico se l'utente rientra nella fascia >65 anni, e se rientra allora applico uno sconto del 40% al prezzo del biglietto
         } else if (userAge > 65) {
             ticketPrice = (userDistance * kmPrice);
-            // - Calcolo il prezzo del biglietto meno lo sconto applicato (in questo caso quello del 40%)
-            ticketPrice = ticketPrice - (ticketPrice * (discountO65 / 100));
             console.log(ticketPrice);
-            // - Grazie all' uso di toFixed 2 posso creare l'output fino a 2 cifre decimali in questo caso
-            ticketPrice = Math.round(ticketPrice * 100) / 100;
+            // - Calcolo il prezzo del biglietto meno lo sconto applicato (in questo caso quello del 40%)
+            ticketPrice = Math.round((ticketPrice - (ticketPrice * (discountO65 / 100))) * 100) / 100;
             console.log(ticketPrice);
             alert(`Abbiamo applicato lo sconto del ${discountO65}%. Il prezzo del biglietto è di ${ticketPrice}€`);
             // - Se l'utente non rientra nelle due fasce non applico nessuno sconto 
         } else {
             ticketPrice = (userDistance * kmPrice);
-            console.log(ticketPrice);
             // - Grazie all' uso di toFixed 2 posso creare l'output fino a 2 cifre decimali in questo caso
             ticketPrice = Math.round(ticketPrice * 100) / 100;
             console.log(ticketPrice);
@@ -55,5 +46,3 @@ if (isNaN(userDistance) || userDistance <= 0 || userDistance === null) { //Verif
         }
     }
 }
-
-// - In tutti i casi creo un output del prezzo del biglietto in forma umana con max 2 cifre decimali 
