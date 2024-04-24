979def9b6221ccaf415310067a50d636cd3fb6b5
MykolaDekusar <ndekusar97@gmail.com>
2024-04-24 14:30:28 +0200

    [UPD] Added user input check

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,20 @@
-'strict mode';
-// - Creo una variabile e Chiedo rramite prompt all'utente il numero di km da percorrere  (verifico che sia effettivamente un numero e lo converto da stringa)
-
+'use strict';
+// - Creo una variabile e Chiedo tramite prompt all'utente il numero di km da percorrere  (verifico che sia effettivamente un numero e lo converto da stringa)
+let userDistance = prompt('Quanti Km vuoi percorrere?');
+console.log(userDistance);
+userDistance = parseInt(userDistance);
+console.log(userDistance);
+if (isNaN(userDistance) || userDistance < 0) {
+    alert('ERRORE Inserisci una distanza valida');
+}
 // - Creo una variabile e Chiedo tramite prompt all'utente l'età  (verifico che sia effettivamente un numero e lo converto da stringa)
+let userAge = prompt('Quanti anni hai?');
+console.log(userAge);
+userAge = parseInt(userAge);
+console.log(userAge);
+if (isNaN(userAge) || userAge < 0) {
+    alert('ERRORE Inserisci un età valida');
+}
 
 // - Creo una variabile del prezzo € al km
 // - Creo una variabile sconto minorenni 20%
